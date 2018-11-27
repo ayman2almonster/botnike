4e9312239866c2f7e70a62711e554cc35d7dd251
ayman2almonster <42792992+ayman2almonster@users.noreply.github.com>
2018-11-28 00:04:02 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,30 @@
+const Eris = require("discord.js");
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);  
+});
+});
+x5bz = "398453811674480641";
+nick = "Ayman ALmonster";
+function onstart(){
+    var v1 = nick.split('');
+    var counter;
+    var i=0;
+var x5bz2 = ' ';
+  var x5bz3 =   setInterval(function(){
+     
+     if (i == v1.length){
+     clearInterval(x5bz3);
+     onstart()
+      return;
+     }
+     var v2 = v1[i];
+    x5bz2 += v2;
+    x5bzteam.editNickname(x5bz, x5bz2);
+ 
+        i++;
+   
+     }, 1000);
+}
+x5bzteam.connect();
+
+client.login(MzU1MTI2NDk2MTY5NDI2OTQ1.Ds4fwg.OLOUmMuXSV3pK3Ae1MGYW2bcS6c);
